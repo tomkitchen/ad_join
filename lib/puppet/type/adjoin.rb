61f05987a7091c2ed9bbafe9c9027ab57f60ae2a
root <root@ip-172-31-41-53.eu-west-1.compute.internal>
2019-09-25 17:57:08 +0000

    testing provider methods

--- lib/puppet/type/adjoin.rb
+++ lib/puppet/type/adjoin.rb
@@ -8,13 +8,13 @@ Puppet::Type.newtype(:adjoin) do
   newparam(:domain, :namevar => true)do
     desc "The name of the domain."
   end
-  newproperty(:user)do
+  newparam(:user)do
     desc "The user name for the domain."
   end
-  newproperty(:password)do
+  newparam(:password)do
     desc "The user password for the domain."
   end
-  newproperty(:port)do
+  newparam(:port)do
     desc "The port for the domain."
   end
 end
